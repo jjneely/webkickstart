71252e85693d9d6c255dbbba09ddc7812ac7aad0
slack <slack@35bbe6b0-b001-0410-9f0e-dd3f93706181>
2002-07-16 21:58:48 +0000

    Added test.py so I could 1) always have the same test suites where ever I work on this, and 2) to test the new location of the cvs source repository
    git-svn-id: https://svn.linux.ncsu.edu/svn/cls/trunk/solaris2ks@927 35bbe6b0-b001-0410-9f0e-dd3f93706181

---
+++ test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from solarisConfig import *
+
+cf = solarisConfig('examples/linux-box')
+#p = cf.parseCommands()
+#for i in p:
+#	print i
+#	
+#print cf.getVersion()
+#print cf.isKickstart()
+#
+#cf = solarisConfig('examples/ks.cfg')
+#print cf.isKickstart()
+
+from baseKickstart import *
+
+ks = baseKickstart(cf)
+ks.includeFile(solarisConfig('examples/pams-server'))
+for i in ks.table:
+	print i
+
