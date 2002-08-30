89faa6f6890041a81b2deb30cd15fc4cd5de3970
slack <slack@35bbe6b0-b001-0410-9f0e-dd3f93706181>
2002-08-30 14:21:27 +0000

    Apache handler
    git-svn-id: https://svn.linux.ncsu.edu/svn/cls/trunk/solaris2ks@959 35bbe6b0-b001-0410-9f0e-dd3f93706181

---
+++ ks.py
@@ -0,0 +1,50 @@
+# ks.py -- apache mod_python handler
+#
+# Copyright, 2002 Jack Neely <slack@quackmaster.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from mod_python import apache
+from mod_python import util
+
+from webKickstart import webKickstart
+
+import sys,os
+
+def handler(req):
+    # Main apache request handler
+    req.content_type = "text/plain"
+    req.send_http_header()
+
+    # set current working dir to something sane
+    os.chdir(sys.path[0])
+
+    # Get the IP of the requestor
+    ip = req.get_remote_host(apache.REMOTE_NOLOOKUP)
+
+    # Init webKickstart
+    w = webKickstart()
+
+    # get the GET/POST thingies
+    args = util.FieldStorage(req)
+    if 'debugtool' in args.keys():
+        tuple = w.getKS(args['debugtool'])
+    else:
+        tuple = w.getKS(ip)
+
+    # send on the kickstart
+    req.write(tuple[1])
+
+    return apache.OK
