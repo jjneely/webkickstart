8f09f17a7679e6cad0e44adbdf4ac8b2907948f9
jjneely@EOS.NCSU.EDU <jjneely@EOS.NCSU.EDU@35bbe6b0-b001-0410-9f0e-dd3f93706181>
2006-06-23 01:45:57 +0000

    Here we go...this properly validates kickstarts.
    git-svn-id: https://svn.linux.ncsu.edu/svn/cls/trunk/solaris2ks@1866 35bbe6b0-b001-0410-9f0e-dd3f93706181

--- debug/validate.py
+++ debug/validate.py
@@ -5,7 +5,13 @@
 import sys
 import urllib2
 import tempfile
-import pykickstart
+
+try:
+    from pykickstart.data import *
+    from pykickstart.parser import *
+except ImportError:
+    print "You need the pykickstart package installed to run this test."
+    sys.exit(1)
 
 url = "http://web-kickstart.linux.ncsu.edu/ks.py"
 
@@ -13,20 +19,37 @@ def main():
     uri = "%s?debugtool=%s" % (url, sys.argv[1])
     ksfd = urllib2.urlopen(uri)
     data = ksfd.read()
-    print data
 
     fileno, filename = tempfile.mkstemp()
     fd = open(filename, 'w')
     fd.write(data)
     fd.close()
 
-    ksdata = pykickstart.data.KickstartData()
-    parser = pykickstart.parser.KickstartParser(ksdata,
-                                         None,
-                                         followIncludes=False,
-                                         errorsAreFatal=True)
-    
-    parser.readKickstart(filename)
+    ksdata = KickstartData()
+    kshandler = KickstartHandlers(ksdata)
+    parser = KickstartParser(ksdata,
+                             kshandler,
+                             followIncludes=False,
+                             errorsAreFatal=True)
+
+    try:
+        parser.readKickstart(filename)
+    except KickstartParseError, e:
+        print "The kickstart is not valid"
+        print "Error message: %s" % str(e)
+
+        try:
+            line = int(str(e).split()[6])
+        except ValueError:
+            line = None
+
+        if line is not None:
+            print "Line %s: %s" % (line, data.split('\n')[line-1])
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) is not 2:
+        print "Usage: %s <hostname>" % sys.argv[0]
+    else:
+        main()
+
