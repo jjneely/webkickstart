f4e886409611053c9d503b7e963a7030d57afafd
jjneely@EOS.NCSU.EDU <jjneely@EOS.NCSU.EDU@35bbe6b0-b001-0410-9f0e-dd3f93706181>
2008-03-05 18:54:33 +0000

    Fix the admins() function to add users to the new sudoers.base so that the enable adminusers and enable normalusers keys do not get wiped out when the afs cron system runs.
    git-svn-id: https://svn.linux.ncsu.edu/svn/cls/branches/solaris2ks/Release-20070628@2634 35bbe6b0-b001-0410-9f0e-dd3f93706181

--- debug/printKS.py
+++ debug/printKS.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-#sys.path.append('../')
+sys.path.append('../')
 #sys.path.append('./')
 import socket
 from webKickstart import webKickstart
@@ -18,8 +18,7 @@ try:
 except:
     #import config
     #from solarisConfig import solarisConfig
-    cfg = config.webksconf()
     sc = solarisConfig(sys.argv[1])
-    gen = cfg.get_obj(sc.getVersion(), {'url': 'http://foo.bar', 'sc': sc})
+    gen = config.get_obj(sc.getVersion(), {'url': 'http://foo.bar', 'sc': sc})
     print gen.makeKS()
 
