e4b94286bce856ad450dd0464645fcf70605df8a
Jack Neely <jjneely@ncsu.edu>
2008-06-25 15:37:22 -0400

    The kickstart parser doesn't understand \ -broken lines.
    Add an rlsecurity.py plugin that will interface with Liquid Dragon.

---
+++ webKickstart/plugins/rlsecurity.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#
+# rlsecurity.py -- Security logging
+#
+# Copyright 2003 - 2008 NC State University
+# Written by Jack Neely <jjneely@pams.ncsu.edu>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import os
+import os.path 
+import xmlrpclib
+import logging
+
+from webKickstart.plugins import WebKickstartPlugin
+from webKickstart.errors  import *
+
+log = logging.getLogger("webks")
+
+class LiquidDragonPlugin(WebKickstartPlugin):
+
+    def run(self):
+        fqdn = str(self.variableDict["webKickstart"].remoteHost)
+        self.loghost(fqdn)
+
+        return self.variableDict
+
+    def loghost(self, fqdn):
+        # Log this install in the DB
+        server = self.cfg.get('main', 'server', None)
+        secret = self.cfg.get('main', 'secret', None)
+
+        if server == None or secret == None:
+            msg = "Missing server or secret values in rlsecurity.conf."
+            raise WebKickstartError, msg
+
+        log.debug("Using XMLRPC API: %s" % server)
+        api = xmlrpclib.ServerProxy(server)
+
+        try:
+            code = api.initHost(secret, fqdn)
+        except Exception, e:
+            raise WebKickstartError("Error initalizing %s with RLM Tools XMLRPC interface.  Halting.\nError: %s" % (fqdn, str(e)))
+
+        if code == 0:
+            return
+        elif code == 1:
+            raise WebKickstartError("Error initalizing %s with RLM Tools XMLRPC interface.  Halting.\nBad authentication." % fqdn)
+        else:
+            raise WebKickstartError("Error initalizing %s with RLM Tools XMLRPC interface.  Halting.\nUnknown error code: %s." % (fqdn, code))
+
+
