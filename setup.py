02da7a4c8f73d35bdce3134a0bc3cf9970e87f74
Jack Neely <jjneely@ncsu.edu>
2008-07-09 14:48:20 -0400

    Add some packaging goodness

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#
+# setup.py - Distutils setup
+#
+# Copyright 2008 NC State University
+# Written by Jack Neely <jjneely@ncsu.edu>
+#
+# SDG
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import os
+import os.path
+from distutils.core import setup
+
+VERSION="3.0"
+
+def getDirs(path):
+    # Split up a directory listing of files and directories into
+    # the python packages to suck in
+    dirs = [path.replace('/', '.')]
+
+    dir = os.listdir(path)
+    for node in dir:
+        apath = os.path.join(path, node)
+        if ".svn" in apath: 
+            continue
+        if ".git" in apath:
+            continue
+        if os.path.isdir(apath):
+            dirs.extend(getDirs(apath))
+
+    return dirs
+
+print getDirs('webKickstart')
+
+setup(  version=VERSION,
+
+        name="Web-Kickstart",
+        description="Dynamically build complex Kickstarts.",
+        author="Jack Neely",
+        author_email="linux@help.ncsu.edu",
+        url="https://secure.linux.ncsu.edu/moin/WebKickstart",
+
+        packages=getDirs('webKickstart'),
+        package_data={'webKickstart': ['webtmpl/*.kid',
+                                       'static/*.png',
+                                       'static/*.gif',
+                                       'static/css/*.css'],
+                     },
+        #data_files=[('/etc/current', ['current.conf'])],
+        scripts=['scripts/makekickstart.py'],
+     )
---
+++ webkickstart.spec
@@ -0,0 +1,55 @@
+%{!?python_sitelib: %define python_sitelib %(%{__python} -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")}
+%{!?python_sitearch: %define python_sitearch %(%{__python} -c "from distutils.sysconfig import get_python_lib; print get_python_lib(1)")}
+
+Name:           webkickstart
+Version:        3.0
+Release:        1%{?dist}
+Summary:        Dynamically generate complex Red Hat Kickstarts.
+
+Group:          System Environment/Daemons
+License:        GPL
+URL:            https://secure.linux.ncsu.edu/moin/WebKickstart
+Source0:        %{name}-%{version}.tar.gz
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+BuildArch:      noarch
+BuildRequires:  python-devel
+
+Requires: python-abi = %(%{__python} -c "import sys ; print sys.version[:3]")
+Requires: mod_python, python-cherrypy, python-kid, python-cheetah
+
+%description
+WebKickstart is an implementation to generate Kickstarts from a Cheetah
+template based off a config file of 'keyword option option ...' lines for
+each client.  Combined with file system sematics you can give different
+groups access to different directories to keep configuration information
+private while making use of a single template.
+
+%prep
+%setup -q
+
+
+%build
+CFLAGS="$RPM_OPT_FLAGS" %{__python} setup.py build
+
+
+%install
+rm -rf $RPM_BUILD_ROOT
+%{__python} setup.py install -O1 --skip-build --root $RPM_BUILD_ROOT
+
+ 
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+
+%files
+%defattr(-,root,root,-)
+%dir /etc/webkickstart
+
+%{_bindir}/*
+%{python_sitelib}/current/*
+
+%changelog
+* Wed Jul 09 2008 Jack Neely <jjneely@ncsu.edu> 3.0-1
+- Initial packaging
+
