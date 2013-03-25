c6017e6b131586a8bc6bdf4247e07b615881c2e4
Jack Neely <jjneely@ncsu.edu>
2013-03-25 14:53:20 -0400

    runserver.py: Let's commit the safter run incatation

--- runserver.py
+++ runserver.py
@@ -4,8 +4,8 @@ def main():
     # Load up the webapp module
     from webKickstart import app
 
-    app.run(host="0.0.0.0", debug=True)
-    #app.run(debug=True)
+    #app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
